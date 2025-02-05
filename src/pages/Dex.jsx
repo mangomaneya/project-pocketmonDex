fefc1ffae09d70864883ee3cade665274b159e17
lee kiri <mango.man.e.ya@gmail.com>
2025-02-05 19:11:53 +0900

    🛠️ 포켓몬 fetch 동작 이동 (Dex to PokemonList)

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,27 +1,8 @@
 import { useState, useEffect } from "react";
 import Dashboard from "../components/Dashboard";
 import PokemonList from "../components/PokemonList";
-import supabase from "../../supabaseClient";
 
 const Dex = () => {
-  const [allPokemon, setAllPokemon] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const { data, error } = await supabase
-        .from("POKEMON_MOCKDATA")
-        .select("*");
-      if (error) {
-        console.log("error =>", error);
-      } else {
-        setAllPokemon(data);
-      }
-    };
-    fetchData();
-    console.log(
-      `포켓몬 리스트를 불러왔고 최초에만 렌더링 됩니다. 이 이후에는 렌더링 되지 않음`
-    );
-  }, []);
-  // console.log(JSON.parse(localStorage.getItem("myPokemons")));
   const [myPokeMons, setMyPokeMons] = useState(
     JSON.parse(localStorage.getItem("myPokemons")) || []
   );
@@ -60,7 +41,7 @@ const Dex = () => {
   return (
     <>
       <Dashboard myPokeMons={myPokeMons} getOutMyPoke={getOutMyPoke} />
-      <PokemonList allPokemon={allPokemon} addInMyPoke={addInMyPoke} />
+      <PokemonList addInMyPoke={addInMyPoke} />
     </>
   );
 };
