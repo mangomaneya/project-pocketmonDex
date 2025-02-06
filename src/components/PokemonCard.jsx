bd69839f36e285d3ce2200ce86750cacba5b15e8
lee kiri <mango.man.e.ya@gmail.com>
2025-02-06 17:28:02 +0900

    :hammer_and_wrench: 쿼리파라미터 수정 / 디테일페이지 포켓몬 추가삭제 구현

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -7,7 +7,7 @@ import { addInMyPoke, getOutMyPoke } from "../redux/pokemonSlice";
 
 const PokemonCard = ({ pokemon, btnName }) => {
   const dispatch = useDispatch();
- 
+
   //prop으로 받은 포켓몬 구조분해할당
   const { id, korean_name, img_url, description, types } = pokemon;
   //디테일페이지 이동 구현
@@ -19,7 +19,7 @@ const PokemonCard = ({ pokemon, btnName }) => {
       return;
     }
     navigate(
-      `/details/value?id=${id}&name=${korean_name}&img=${img_url}&desc=${description}&types=${types}`,
+      `/details/pokemon?id=${id}`,
       { state: { pokemon } } //포켓몬 스테이트 전달
     );
   };
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -2,7 +2,8 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { CardBtn } from "../components/PokemonCard";
 import styled from "styled-components";
 import { useDispatch } from "react-redux";
-import { addInMyPoke } from "../redux/pokemonSlice";
+import { addInMyPoke, getOutMyPoke } from "../redux/pokemonSlice";
+import { useSelector } from "react-redux";
 
 const Details = () => {
   const dispatch = useDispatch();
@@ -12,21 +13,22 @@ const Details = () => {
   const queryParams = new URLSearchParams(location.search);
   // 변수에 쿼리 파라미터 할당
   const id = queryParams.get("id");
-  const name = queryParams.get("name");
-  const img = queryParams.get("img");
-  const desc = queryParams.get("desc");
-  const types = queryParams.get("types");
-  // 포켓몬 추가 기능용 스테이트 설정
   const { pokemon } = location.state || {};
-  // window.history.replaceState({}, null, location.pathname); url 주소의 쿼리 숨기기 - 새로고침 시 오류 발생되어 취소
-
+  const { korean_name, img_url, description, types } = pokemon;
+  //해당페이지 포켓몬이 마이포켓몬에 포함되어있는지 체크
+  const myPokemons = useSelector((state) => state.myPokemonList.myPokemons);
+  // console.log('myPokemons', myPokemons)
+  const isContain = myPokemons.some((pokemons) => {
+    return pokemons.id === pokemon.id;
+  });
+  console.log("isContain", isContain);
   return (
     <StDiv>
-      <p>{name}</p>
-      <img src={img} />
+      <p>{korean_name}</p>
+      <img src={img_url} />
       <p>{id < 10 ? `No.00${id}` : id < 100 ? `No.0${id}` : `N.0${id}`}</p>
       <p>{types}</p>
-      <p>{desc}</p>
+      <p>{description}</p>
       {/* 덱스페이지로 돌아가기 */}
       <CardBtn
         onClick={() => {
@@ -35,7 +37,15 @@ const Details = () => {
       >
         뒤로가기
       </CardBtn>
-      <CardBtn onClick={() => dispatch(addInMyPoke(pokemon))}>추가하기</CardBtn>
+      {isContain ? (
+        <CardBtn onClick={() => dispatch(getOutMyPoke(pokemon))}>
+          삭제하기
+        </CardBtn>
+      ) : (
+        <CardBtn onClick={() => dispatch(addInMyPoke(pokemon))}>
+          추가하기
+        </CardBtn>
+      )}
     </StDiv>
   );
 };
